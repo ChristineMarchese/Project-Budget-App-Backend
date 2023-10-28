6af5a94baa79d81b0a795f6fe1aee33d0a8f1b97
Christine Marchese <chm978@gmail.com>
2023-10-28 13:07:21 -0400

    commit-3

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const app = express; //calling the express function
+const app = express(); //calling the express function
+
+const cors = require("cors");//importing cors connects the backend with frontend
+
+app.use(cors());
+
+const budgetController = require("./controllers/budgetController");
+
+app.use("/budget", budgetController);
 
 
-const cors = require("cors");
-app.request(cors());
 
-
-
-app.length("/", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello World");
-})
+});
+
+app.get("*", (req, res) => {
+    res.status(404).send("Not Found");
+});
 
 
 
---
+++ controllers/budgetController.js
@@ -0,0 +1,49 @@
+const express = require("express");
+
+const budget = express.Router()// setup the router
+
+const budgetArray = require("../models/budget.js");
+
+budget.get("/", (req, res) => {
+     res.send(budgetArray);
+});
+
+budget.get("/:id", (req, res) => {
+    const id = req.params.id;
+    if(budgetArray[id]){
+       res.send(budgetArray[id]);
+    } else {
+      res.status(404).send("message: not found")
+    }
+});
+
+budget.post("/", express.json(), (req, res) => {
+    if(req.body){
+      budgetArray.push(req.body);// creating the new item and push into the array
+      res.send(budgetArray);
+    } else {
+       res.status(404).send("message: not found");
+    }
+});
+
+budget.delete("/:id", (req, res) => {
+     const id = req.params.id;
+     if(!budgetArray){
+        res.status(404).send("message: not found");
+     } else {
+       budgetArray.splice(id, 1);
+       res.send(budgetArray);
+     }
+});
+
+budget.put("/:id", express.json(), (req, res) => {
+    const id = req.params.id;
+    if(budgetArray[id]){
+        budgetArray[id] = req.body;
+        res.send(budgetArray[id]);
+    } else {
+        res.status(404).send("message: not message")
+    }
+});
+
+module.exports = budget
